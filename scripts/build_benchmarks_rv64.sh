3e50761a0f2546a38d01e54623d225c3b8ed2bed
AJASU <junyoung090928@gmail.com>
2026-09-19 02:19:05 +0900

    test(rv64): build directed N-SKIP gap matrix binaries

--- scripts/build_benchmarks_rv64.sh
+++ scripts/build_benchmarks_rv64.sh
@@ -37,3 +37,21 @@ file benchmarks/bin/rv64_intdiv_smoke
   benchmarks/src/rv64_div_fairness.S
 
 file benchmarks/bin/rv64_div_fairness
+
+
+for gap in 0 1 2 3 4 5 6 7 8; do
+  "${CC}" \
+    -O2 \
+    -static \
+    -nostdlib \
+    -nostartfiles \
+    -march=rv64imafd \
+    -mabi=lp64d \
+    -DGAP="${gap}" \
+    -Wl,-e,_start \
+    -o "benchmarks/bin/rv64_gap_${gap}" \
+    benchmarks/src/rv64_gap_sweep.S
+done
+
+file benchmarks/bin/rv64_gap_0
+file benchmarks/bin/rv64_gap_8
